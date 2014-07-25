925b1ce5ae02446e7609848a1253c4c215ef2b60
im_ZEH <pi.neilr23@gmail.com>
2014-07-25 12:54:52 -0700

    Second Commit

--- server.js
+++ server.js
@@ -22,16 +22,6 @@ mongoose.connect('mongodb://localhost/Node');
 
 app.use(express.static(__dirname + '/templates'));
 
-
-// app.get('*', function(req, res){
-// 	res.sendfile('index.html');
-// })
-
-// router.route('/')
-// 	.get(function(req, res) {
-// 		res.sendfile('index.hmtl');
-// 	});
-
 router.use(function(req, res, next){
 	console.log('Something is happening');
 	next();
@@ -95,53 +85,6 @@ router.route('/phones/:phone_id')
 		});
 	});	
 
-// 	router.get('/', function(req, res){
-// 	res.json({ message: 'hooray! welcome to our api!'});
-// });
-
-
 app.use('/api', router);
 app.listen(port);
 console.log('App listening on port '+ port);
-
-// app.get('/phones', function(req, res){
-// 	Todo.find(function(err, phones){
-// 		if(err)
-// 			res.send(err)
-// 		res.json(phones);
-// 	});
-// });
-
-// app.post('/phones', function(req, res){
-// 	Todo.create({
-// 		text : req.body.text,
-// 		done: false
-// 	}, function(err, todo){
-// 		if(err)
-// 			res.send(err);
-
-// 		Todo.find(function(err, phones){
-// 			if(err)
-// 				res.send(err)
-// 			res.json(phones);
-// 		});
-// 	});
-// });
-
-
-// app.delete('/phones/:todo_id', function(req, res){
-// 	Todo.remove({
-// 		_id : req.params.todo_id
-// 	}, function(err, todo){
-// 		if(err)
-// 			res.send(err);
-
-// 		Todo.find(function(err, phones){
-// 			if(err)
-// 				res.send(err)
-// 			res.json(phones);
-// 		});
-// 	});
-// });
-
-
